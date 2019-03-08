3467a2bb1b08ff5472d08f7d0e71467c0f722dc4
r <rmijic@gmail.com>
2019-03-08 13:44:09 +0100

    work on ccattack begins

--- main/engine/closecombat/ccattack.py
+++
@@ -1,8 +0,0 @@
-from engine.data.model import Model
-
-class CcAttack:
-
-    def attack(attacker: Model, numattackers, numsuppattackers, defender: Model, numdefenders):
-
-
-    def
---
+++ src/main/engine/closecombat/ccattack.py
@@ -0,0 +1,10 @@
+from engine.data.model import Model
+
+class CcAttack:
+
+    pass
+    #
+    # def attack(attacker: Model, numattackers, numsuppattackers, defender: Model, numdefenders):
+    #
+    #
+    # def
