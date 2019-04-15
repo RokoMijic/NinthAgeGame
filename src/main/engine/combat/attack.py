f39c800a51d75927879f1feb0858650779c7ad3a
r <rmijic@gmail.com>
2019-04-15 21:13:45 +0200

    updates to attack/hit code

--- src/main/engine/combat/attack.py
+++ src/main/engine/combat/attack.py
@@ -4,22 +4,40 @@ from src.main.engine.data.unit import Unit
 from src.main.engine.constants import TO_HIT_TABLE
 from src.main.engine.utility.utiliy import n_d6_oversix
 import numpy as np
+import logging
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+logger.addHandler(logging.StreamHandler())
+
 
 class Attack:
 
     @staticmethod
     def attack_unit(attacker: Unit, defender: Unit, facing: str = "FRONT"):
+
         pass
 
     @staticmethod
-    def attack_single(attackingmp: ModelPart, defender: Model):
+    def calculate_n_attacks(attacker: Unit, defender: Unit, facing: str):
         pass
+
+
+    @staticmethod
+    def depth(Unit):
+        n_models = Unit.number
+        width = Unit.width
+
 
     @staticmethod
     def roll_to_hit(attackingmp: ModelPart, defender: Model, numattacks: int = 1):
-        off_def_diff = attackingmp.v("Off") - defender.v("Def")
+        off = attackingmp.v("Off")
+        defence = defender.v("Def")
+        off_def_diff = off - defence
         hit_oversix = TO_HIT_TABLE[np.clip(off_def_diff, a_min=-10, a_max=10)]
-        return sum( n_d6_oversix(hit_oversix,numattacks) )
+        total_hits = sum(n_d6_oversix(numattacks, hit_oversix))
+        logger.debug("%s with Off %s attacking %s with Def %s, %s times with probability %s/6, hit %s times" % (attackingmp.modelpartname, off, defender.modelname, defence, numattacks, hit_oversix, total_hits)  )
+        return total_hits
 
 
 
--- src/main/engine/utility/utiliy.py
+++ src/main/engine/utility/utiliy.py
@@ -1,5 +1,7 @@
 import numpy as np
 
 
-def n_d6_oversix(X: int, n:int):
-    return 1*(np.random.randint(1,6,n) <= X)
+def n_d6_oversix(n: int, X: int):
+    # Roll n binary variables with probability X/6 of succeeding
+    return 1*(np.random.randint(1,6+1,n) <= X)
+
