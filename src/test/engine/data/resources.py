5cd7aacec4c5297b8d0a91f8c45083f0ad2387cf
r <rmijic@gmail.com>
2019-04-16 18:05:27 +0200

    Added knight model resource

--- src/test/engine/data/resources.py
+++ src/test/engine/data/resources.py
@@ -9,8 +9,12 @@ from src.main.engine.data.modelpart import ModelPart
 # OFFENSIVE_STAT_NAMES = [ "Att", "Off", "Str", "AP", "Agi" ]
 
 swordmodelpart = ModelPart("swordattack", [1, 3, 3, 0, 3], ["HATRED"])
-swordsmanmodel = Model("swordsman", [4, 8, 7, 1, 3, 3, 2],  ["PARRY"], [ModelPart("swordattack", [1, 3, 3, 0, 3], ["HATRED"])] )
+swordsmanmodel = Model("swordsman", [4, 8, 7, 1, 3, 3, 2],  ["PARRY"], [ swordmodelpart ] )
 
 zombiemodelpart = ModelPart("zombieslap", [1, 1, 3, 0, 2], ["POISON"])
-zombiemodel = Model("zombie", [4, 8, 7, 1, 1, 4, 0],  ["FEARLESS"], [ModelPart("zombieslap", [1, 1, 3, 0, 2], ["POISON"])] )
+zombiemodel = Model("zombie", [4, 8, 7, 1, 1, 4, 0],  ["FEARLESS"], [ zombiemodelpart ] )
+
+knightmodelpart = ModelPart("knightattck", [1, 4, 4, 1, 4],[])
+horsemodelpart = ModelPart("horsekick", [1, 3, 3, 0, 3], ["HARNESSED"])
+knightmodel = Model("knight", [8, 16, 8, 1, 4, 3, 5],  [], [ knightmodelpart , horsemodelpart ] )
 
