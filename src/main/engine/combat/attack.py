3694049da1ce95393b597113e469f93b9e95faab
r <rmijic@gmail.com>
2019-04-16 17:36:21 +0200

    Implemented attacks and tests thereof

--- src/main/engine/combat/attack.py
+++ src/main/engine/combat/attack.py
@@ -1,7 +1,7 @@
 from src.main.engine.data.model import Model
 from src.main.engine.data.modelpart import ModelPart
 from src.main.engine.data.unit import Unit
-from src.main.engine.constants import TO_HIT_TABLE
+from src.main.engine.constants import TO_HIT_TABLE, TO_WOUND_TABLE
 from src.main.engine.utility.utiliy import n_d6_oversix
 import numpy as np
 import logging
@@ -15,7 +15,6 @@ class Attack:
 
     @staticmethod
     def attack_unit(attacker: Unit, defender: Unit, facing: str = "FRONT"):
-
         pass
 
     @staticmethod
@@ -34,12 +33,59 @@ class Attack:
         off = attackingmp.v("Off")
         defence = defender.v("Def")
         off_def_diff = off - defence
-        hit_oversix = TO_HIT_TABLE[np.clip(off_def_diff, a_min=-10, a_max=10)]
-        total_hits = sum(n_d6_oversix(numattacks, hit_oversix))
-        logger.debug("%s with Off %s attacking %s with Def %s, %s times with probability %s/6, hit %s times" % (attackingmp.modelpartname, off, defender.modelname, defence, numattacks, hit_oversix, total_hits)  )
-        return total_hits
+        success_prob_oversix = TO_HIT_TABLE[np.clip(off_def_diff, a_min=-10, a_max=10)]
+        total_successes = sum(n_d6_oversix(numattacks, success_prob_oversix))
+        logger.debug("%s with Off %s attacking %s with Def %s, %s times with probability %s/6, hit %s times" % \
+                     (attackingmp.modelpartname, off, defender.modelname, defence, numattacks, success_prob_oversix, total_successes))
+        return total_successes
 
 
+    @staticmethod
+    def roll_to_wound(attackingmp: ModelPart, defender: Model, numhits: int = 1):
+        str = attackingmp.v("Str")
+        res = defender.v("Res")
+        str_res_diff = str - res
+        success_prob_oversix = TO_WOUND_TABLE[np.clip(str_res_diff, a_min=-10, a_max=10)]
+        total_successes = sum(n_d6_oversix(numhits, success_prob_oversix))
+        logger.debug("%s with Str %s hits %s with Res %s, %s times with probability %s/6, wounds %s times" % \
+                     (attackingmp.modelpartname, str, defender.modelname, res, numhits, success_prob_oversix, total_successes))
+        return total_successes
+
+    @staticmethod
+    def roll_to_penetrate(attackingmp: ModelPart, defender: Model, numwounds: int = 1):
+        ap = attackingmp.v("AP")
+        arm = defender.v("Arm")
+        arm_pen_diff = arm-ap
+        success_prob_oversix = 6-np.clip(arm_pen_diff, a_min=0, a_max=5)
+        total_successes = sum(n_d6_oversix(numwounds, success_prob_oversix))
+        logger.debug("%s with AP %s wounds %s with Arm %s, %s times with probability %s/6, total of %s unsaved wounds" % \
+                     (attackingmp.modelpartname, ap, defender.modelname, arm, numwounds, success_prob_oversix, total_successes))
+        return total_successes
+
+    @staticmethod
+    def roll_against_special_save(attackingmp: ModelPart, defender: Model, num_unsaved_wounds: int = 1):
+
+        # TODO: make special saves work
+        total_successes = num_unsaved_wounds
+
+        logger.debug("%s gets unsaved wounds against %s, %s get through the special save TODO THESE SAVES ARE NOT BEING IMPLEMENTED" % \
+                     (attackingmp.modelpartname, defender.modelname, total_successes ))
+        return total_successes
+
+
+    @staticmethod
+    def full_attack_roll(attackingmp: ModelPart, defender: Model, numattacks: int = 1):
+
+        total_successes = Attack.roll_against_special_save(attackingmp, defender,
+                                    Attack.roll_to_penetrate(attackingmp, defender,
+                                                Attack.roll_to_wound(attackingmp, defender,
+                                                                Attack.roll_to_hit(attackingmp, defender,numattacks)
+                                                                    )
+                                                            )
+                                                          )
+
+        logger.debug("%s attacks %s, %s times, total of %s unsaved wounds" % \
+                     (attackingmp.modelpartname, defender.modelname, numattacks, total_successes))
 
 
 
--- src/test/engine/combat/testattack.py
+++ src/test/engine/combat/testattack.py
@@ -5,6 +5,9 @@ import logging
 
 class TestAttack(unittest.TestCase):
 
+
+    # Test to hit
+
     def test_to_hit(self):
         Attack.roll_to_hit(swordmodelpart, zombiemodel )
 
@@ -13,6 +16,31 @@ class TestAttack(unittest.TestCase):
 
     def test_to_hit_1000_zombine_attacks_swordsman(self):
         Attack.roll_to_hit(zombiemodelpart, swordsmanmodel, numattacks=10000)
+
+    #     test to wound
+
+    def test_to_wound_1000(self):
+        Attack.roll_to_wound(swordmodelpart, zombiemodel, numhits=10000)
+
+    def test_to_wound_1000_zombine_attacks_swordsman(self):
+        Attack.roll_to_wound(zombiemodelpart, swordsmanmodel, numhits=10000)
+
+    #     test armour penetration
+
+    def test_to_penetrate_1000(self):
+        Attack.roll_to_penetrate(swordmodelpart, zombiemodel, numwounds=10000)
+
+    def test_to_penetrate_1000_zombine_attacks_swordsman(self):
+        Attack.roll_to_penetrate(zombiemodelpart, swordsmanmodel, numwounds=10000)
+
+    #     test full attack roll
+
+    def test_full_attack_1000(self):
+        Attack.full_attack_roll(swordmodelpart, zombiemodel, numattacks=10000)
+
+    def test_full_attack_1000_zombine_attacks_swordsman(self):
+        Attack.full_attack_roll(zombiemodelpart, swordsmanmodel, numattacks=10000)
+
 
 
 if __name__ == '__main__':
